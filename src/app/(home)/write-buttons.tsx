a8e21757e1b71b1f5d6270f34c922500aa7011c0
evyohyeah <99656663+evyohyeah@users.noreply.github.com>
2026-05-14 18:04:50 +0800

    Update write-buttons.tsx
    簡轉繁

--- src/app/(home)/write-buttons.tsx
+++ src/app/(home)/write-buttons.tsx
@@ -54,7 +54,7 @@ export default function WriteButton() {
 					)}
 
 					<PenSVG />
-					<span>写文章</span>
+					<span>寫文章</span>
 				</motion.button>
 				<motion.button
 					initial={{ opacity: 0, scale: 0.6 }}
